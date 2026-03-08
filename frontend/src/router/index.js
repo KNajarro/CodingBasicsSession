e685baefd5db53fb54c5c2ef93613a0f60a5b47e
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-03-08 15:42:52 -0600

    Added dashboard path in routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView     from '../views/HomeView.vue'
-import PeopleView   from '../views/PeopleView.vue'
-import ProductsView from '../views/ProductsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PeopleView from "../views/PeopleView.vue";
+import ProductsView from "../views/ProductsView.vue";
+import DashboardView from "../views/DashboardView.vue";
 
 const routes = [
-  { path: '/',         name: 'home',     component: HomeView     },
-  { path: '/people',   name: 'people',   component: PeopleView   },
-  { path: '/products', name: 'products', component: ProductsView }
-]
+  { path: "/", name: "home", component: HomeView },
+  { path: "/people", name: "people", component: PeopleView },
+  { path: "/products", name: "products", component: ProductsView },
+  { path: "/dashboard", name: "dashboard", component: DashboardView },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
