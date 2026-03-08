fa6f274dcedd20c249035c9737e77449251d60dd
Jeffrey Alexander Argueta Galicia <argueta.galicia@outlook.com>
2026-03-07 22:53:25 -0600

    fix: api port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // All requests starting with /api are forwarded to the .NET API
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5261',
         changeOrigin: true
       }
     }
