6db9f32817ed0880aa7414db6e8da4474daac5f6
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-03-08 03:28:32 -0600

    Enable Api services

--- frontend/src/services/productsService.js
+++ frontend/src/services/productsService.js
@@ -1,4 +1,4 @@
-import api from './api'
+import api from "./api";
 
 /**
  * Products Service
@@ -7,40 +7,35 @@ import api from './api'
 export const productsService = {
   /** GET /api/products - returns all ProductDto records */
   async getAll() {
-    // TODO: const response = await api.get('/products')
-    //       return response.data
-    throw new Error('Workshop: Implement getAll()')
+    const response = await api.get("/products");
+    return response.data;
   },
 
   /** GET /api/products/search?name=&categoryName= */
   async search(name, categoryName) {
-    // TODO: const params = {}
-    //       if (name)         params.name         = name
-    //       if (categoryName) params.categoryName = categoryName
-    //       const response = await api.get('/products/search', { params })
-    //       return response.data
-    throw new Error('Workshop: Implement search()')
+    const params = {};
+    if (name) params.name = name;
+    if (categoryName) params.categoryName = categoryName;
+    const response = await api.get("/products/search", { params });
+    return response.data;
   },
 
   /** POST /api/products - create a new product */
   async create(product) {
-    // TODO: const response = await api.post('/products', product)
-    //       return response.data
-    throw new Error('Workshop: Implement create()')
+    const response = await api.post("/products", product);
+    return response.data;
   },
 
   /** PUT /api/products/{id} - update a product */
   async update(id, product) {
-    // TODO: const response = await api.put(`/products/${id}`, product)
-    //       return response.data
-    throw new Error('Workshop: Implement update()')
+    const response = await api.put(`/products/${id}`, product);
+    return response.data;
   },
 
   /** DELETE /api/products/{id} - delete a product */
   async delete(id) {
-    // TODO: await api.delete(`/products/${id}`)
-    throw new Error('Workshop: Implement delete()')
-  }
-}
+    await api.delete(`/products/${id}`);
+  },
+};
 
-export default productsService
+export default productsService;
