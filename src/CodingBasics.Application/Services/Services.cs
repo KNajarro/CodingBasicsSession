283e6ced9127fb496d71a13090fcec775d099bd4
Guillermo Eguizabal <cgeguizabal@gmail.com>
2026-03-08 00:37:10 -0600

    Completed PersonService and ProductService CRUD

--- src/CodingBasics.Application/Services/Services.cs
+++ src/CodingBasics.Application/Services/Services.cs
@@ -1,4 +1,7 @@
+using System.Formats.Asn1;
 using CodingBasics.Domain.Contracts;
+using CodingBasics.Domain.AdventureWorks.Entities;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CodingBasics.Application.Services;
 
@@ -17,17 +20,122 @@ public sealed class PersonService : IPersonService
 
     public async Task<IEnumerable<PersonDto>> GetAllAsync(CancellationToken ct = default)
     {
-        return await _repository.GetAllAsync(ct);
-    }
-
-    public Task<IEnumerable<PersonDto>> SearchAsync(string? name, string? personType, CancellationToken ct = default)
+        var people = await _repository.GetAllAsync(ct);
+        return people.Select(p => new PersonDto
     {
-        // TODO: return _repository.SearchAsync(name, personType, ct);
-        throw new NotImplementedException("Workshop: Implement SearchAsync");
+        BusinessEntityID = p.BusinessEntityID,
+        PersonType = p.PersonType,
+        NameStyle = p.NameStyle,
+        Title = p.Title,
+        FirstName = p.FirstName,
+        MiddleName = p.MiddleName,
+        LastName = p.LastName,
+        Suffix = p.Suffix,
+        EmailPromotion = p.EmailPromotion,
+        AdditionalContactInfo = p.AdditionalContactInfo,
+        Demographics = p.Demographics
+    });
     }
 
-    // TODO: Implement CreateAsync, UpdateAsync, DeleteAsync for Person
+    public async Task<IEnumerable<PersonDto>> SearchAsync(string? name, string? personType, CancellationToken ct = default)
+    {
+        var people = await _repository.SearchAsync(name, personType, ct);
+
+        return people.Select(p => new PersonDto
+        {
+            BusinessEntityID = p.BusinessEntityID,
+            PersonType = p.PersonType,
+            NameStyle = p.NameStyle,
+            Title = p.Title,
+            FirstName = p.FirstName,
+            MiddleName = p.MiddleName,
+            LastName = p.LastName,
+            Suffix = p.Suffix,
+            EmailPromotion = p.EmailPromotion,
+            AdditionalContactInfo = p.AdditionalContactInfo,
+            Demographics = p.Demographics
+        });
+    }
+
+   public async Task<PersonDto> CreateAsync(PersonDto dto, CancellationToken ct = default)
+    {
+        var person = new Person
+        {
+            BusinessEntityID = dto.BusinessEntityID,
+            PersonType = dto.PersonType,
+            NameStyle = dto.NameStyle,
+            Title = dto.Title,
+            FirstName = dto.FirstName,
+            MiddleName = dto.MiddleName,
+            LastName = dto.LastName,
+            Suffix = dto.Suffix,
+            EmailPromotion = dto.EmailPromotion,
+            AdditionalContactInfo = dto.AdditionalContactInfo,
+            Demographics = dto.Demographics
+        };
+
+        var createdPerson = await _repository.CreateAsync(person, ct);
+
+        return new PersonDto
+        {
+            BusinessEntityID = createdPerson.BusinessEntityID,
+            PersonType = createdPerson.PersonType,
+            NameStyle = createdPerson.NameStyle,
+            Title = createdPerson.Title,
+            FirstName = createdPerson.FirstName,
+            MiddleName = createdPerson.MiddleName,
+            LastName = createdPerson.LastName,
+            Suffix = createdPerson.Suffix,
+            EmailPromotion = createdPerson.EmailPromotion,
+            AdditionalContactInfo = createdPerson.AdditionalContactInfo,
+            Demographics = createdPerson.Demographics
+        };
+    }
+
+    public async Task<PersonDto> UpdateAsync(int id, PersonDto dto, CancellationToken ct = default)
+    {
+        var person = new Person
+        {
+            BusinessEntityID = dto.BusinessEntityID,
+            PersonType = dto.PersonType,
+            NameStyle = dto.NameStyle,
+            Title = dto.Title,
+            FirstName = dto.FirstName,
+            MiddleName = dto.MiddleName,
+            LastName = dto.LastName,
+            Suffix = dto.Suffix,
+            EmailPromotion = dto.EmailPromotion,
+            AdditionalContactInfo = dto.AdditionalContactInfo,
+            Demographics = dto.Demographics
+        };
+
+        var updatedPerson = await _repository.UpdateAsync(id, person, ct);
+
+        return new PersonDto
+        {
+            BusinessEntityID = updatedPerson.BusinessEntityID,
+            PersonType = updatedPerson.PersonType,
+            NameStyle = updatedPerson.NameStyle,
+            Title = updatedPerson.Title,
+            FirstName = updatedPerson.FirstName,
+            MiddleName = updatedPerson.MiddleName,
+            LastName = updatedPerson.LastName,
+            Suffix = updatedPerson.Suffix,
+            EmailPromotion = updatedPerson.EmailPromotion,
+            AdditionalContactInfo = updatedPerson.AdditionalContactInfo,
+            Demographics = updatedPerson.Demographics
+        };
+    }
+
+    public Task DeleteAsync(int id, CancellationToken ct = default)
+    {
+        
+        return _repository.DeleteAsync(id, ct);
+    }
+
 }
+
+    
 
 /// <summary>
 /// Product service implementation.
